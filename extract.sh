d125c895cba766775955da2a6a846ca2eaede4fa
redmaner <jakevdputten@gmail.com>
2014-02-27 19:55:32 +0100

    Rewrite extract.sh

--- extract.sh
+++ extract.sh
@@ -20,9 +20,10 @@ esac
 
 LOG=error.log
 MAIN_DEVICE=aries
+PYTHON=python2
 
-rm -f $LOG
-exec 2>> $LOG
+#rm -f $LOG
+#exec 2>> $LOG
 
 mkdir -p $PWD/.cache
 
@@ -49,7 +50,7 @@ mkdir -p .cache/ROM
 if [ -e $ZIP ]; then
      	cp $ZIP .cache/ROM/$ZIP
 else
-     	echo "Error: no $ZIP found"; exit
+     	echo "Error: $ZIP not found"; exit
 fi
 
 echo -e "${txtblu}Extracting zip${txtrst}: $ZIP"
@@ -59,33 +60,33 @@ tools/apktool if .cache/ROM/system/framework/framework-res.apk
 tools/apktool if .cache/ROM/system/framework/framework-miui-res.apk
 }
 
-extract_xmls_main () {
+extract_xmls () {
 I=0
-TOTAL=$(wc -l targets/main.apks | cut -d' ' -f1)
-cat targets/main.apks | while read all_line; do
+TOTAL=$(wc -l $TARGET_FILE | cut -d' ' -f1)
+cat $TARGET_FILE | while read all_line; do
         I=$(expr $I + 1)
         PERCENT=$(echo "scale=2; $I/$TOTAL*100" | bc)
     	if [ -e .cache/ROM/system/app/$all_line ]; then
                 echo -en "${txtblu}Extracting main XML's${txtrst}: $PERCENT%\r"
     		echo >> $LOG; echo "Extracting $all_line" >> $LOG
     		tools/apktool d -f .cache/ROM/system/app/$all_line .cache/apk_wip
-    		mkdir -p Dev/main/$all_line/res/values
-    		if [ -e .cache/apk_wip/res/values/strings.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/strings.xml > .cache/apk_wip/res/values/strings.xml.new
-			if [ -e .cache/apk_wip/res/values/strings.xml.new ]; then
-         			cp .cache/apk_wip/res/values/strings.xml.new Dev/main/$all_line/res/values/strings.xml
+    		mkdir -p Dev/$TARGET_DIR/$all_line/res/values$ISO
+    		if [ -e .cache/apk_wip/res/values$ISO/strings.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/strings.xml > .cache/apk_wip/res/values$ISO/strings.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/strings.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/strings.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/strings.xml
 			fi
     		fi
-    		if [ -e .cache/apk_wip/res/values/arrays.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/arrays.xml > .cache/apk_wip/res/values/arrays.xml.new
-			if [ -e .cache/apk_wip/res/values/arrays.xml.new ]; then
-         			cp .cache/apk_wip/res/values/arrays.xml.new Dev/main/$all_line/res/values/arrays.xml
+    		if [ -e .cache/apk_wip/res/values$ISO/arrays.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/arrays.xml > .cache/apk_wip/res/values$ISO/arrays.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/arrays.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/arrays.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/arrays.xml
 			fi
     		fi
-    		if [ -e .cache/apk_wip/res/values/plurals.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/plurals.xml > .cache/apk_wip/res/values/plurals.xml.new
-			if [ -e .cache/apk_wip/res/values/plurals.xml.new ]; then
-         			cp .cache/apk_wip/res/values/plurals.xml.new Dev/main/$all_line/res/values/plurals.xml
+    		if [ -e .cache/apk_wip/res/values$ISO/plurals.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/plurals.xml > .cache/apk_wip/res/values$ISO/plurals.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/plurals.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/plurals.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/plurals.xml
 			fi
     		fi
 		echo -en "\r${txtwipe}"
@@ -93,126 +94,76 @@ cat targets/main.apks | while read all_line; do
                 echo -en "${txtblu}Extracting main XML's${txtrst}: $PERCENT%\r"
     		echo >> $LOG; echo "Extracting $all_line" >> $LOG
     		tools/apktool d -f .cache/ROM/system/framework/$all_line .cache/apk_wip
-    		mkdir -p Dev/main/$all_line/res/values
-    		if [ -e .cache/apk_wip/res/values/strings.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/strings.xml > .cache/apk_wip/res/values/strings.xml.new
-			if [ -e .cache/apk_wip/res/values/strings.xml.new ]; then
-         			cp .cache/apk_wip/res/values/strings.xml.new Dev/main/$all_line/res/values/strings.xml
+    		mkdir -p Dev/$TARGET_DIR/$all_line/res/values$ISO
+    		if [ -e .cache/apk_wip/res/values$ISO/strings.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/strings.xml > .cache/apk_wip/res/values$ISO/strings.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/strings.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/strings.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/strings.xml
 			fi
     		fi
-    		if [ -e .cache/apk_wip/res/values/arrays.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/arrays.xml > .cache/apk_wip/res/values/arrays.xml.new
-			if [ -e .cache/apk_wip/res/values/arrays.xml.new ]; then
-         			cp .cache/apk_wip/res/values/arrays.xml.new Dev/main/$all_line/res/values/arrays.xml
+    		if [ -e .cache/apk_wip/res/values$ISO/arrays.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/arrays.xml > .cache/apk_wip/res/values$ISO/arrays.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/arrays.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/arrays.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/arrays.xml
 			fi
     		fi
-    		if [ -e .cache/apk_wip/res/values/plurals.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/plurals.xml > .cache/apk_wip/res/values/plurals.xml.new
-			if [ -e .cache/apk_wip/res/values/plurals.xml.new ]; then
-         			cp .cache/apk_wip/res/values/plurals.xml.new Dev/main/$all_line/res/values/plurals.xml
-			fi
-    		fi
-		echo -en "\r${txtwipe}"
-	fi
-done
-tools/sort.py Dev/main
-echo -e "${txtgrn}Extracted main XML's${txtrst}"
-}
-
-extract_xmls_device () {
-I=0
-TOTAL=$(wc -l targets/$TARGET.apks | cut -d' ' -f1)
-cat targets/$TARGET.apks | while read all_line; do
-        I=$(expr $I + 1)
-        PERCENT=$(echo "scale=2; $I/$TOTAL*100" | bc)
-    	if [ -e .cache/ROM/system/app/$all_line ]; then
-                echo -en "${txtblu}Extracting $TARGET XML's${txtrst}: $PERCENT%\r"
-    		echo >> $LOG; echo "Extracting $all_line" >> $LOG
-    		tools/apktool d -f .cache/ROM/system/app/$all_line .cache/apk_wip
-    		mkdir -p Dev/device/$TARGET/$all_line/res/values
-    		if [ -e .cache/apk_wip/res/values/strings.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/strings.xml > .cache/apk_wip/res/values/strings.xml.new
-			if [ -e .cache/apk_wip/res/values/strings.xml.new ]; then
-         			cp .cache/apk_wip/res/values/strings.xml.new Dev/device/$TARGET/$all_line/res/values/strings.xml
-			fi
-    		fi
-    		if [ -e .cache/apk_wip/res/values/arrays.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/arrays.xml > .cache/apk_wip/res/values/arrays.xml.new
-			if [ -e .cache/apk_wip/res/values/arrays.xml.new ]; then
-         			cp .cache/apk_wip/res/values/arrays.xml.new Dev/device/$TARGET/$all_line/res/values/arrays.xml
-			fi
-    		fi
-    		if [ -e .cache/apk_wip/res/values/plurals.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/plurals.xml > .cache/apk_wip/res/values/plurals.xml.new
-			if [ -e .cache/apk_wip/res/values/plurals.xml.new ]; then
-         			cp .cache/apk_wip/res/values/plurals.xml.new Dev/device/$TARGET/$all_line/res/values/plurals.xml
-			fi
-    		fi
-		echo -en "\r${txtwipe}"
-    	elif [ -e .cache/ROM/system/framework/$all_line ]; then
-                echo -en "${txtblu}Extracting $TARGET XML's${txtrst}: $PERCENT%\r"
-    		echo >> $LOG; echo "Extracting $all_line" >> $LOG
-    		tools/apktool d -f .cache/ROM/system/framework/$all_line .cache/apk_wip
-    		mkdir -p Dev/device/$TARGET/$all_line/res/values
-    		if [ -e .cache/apk_wip/res/values/strings.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/strings.xml > .cache/apk_wip/res/values/strings.xml.new
-			if [ -e .cache/apk_wip/res/values/strings.xml.new ]; then
-         			cp .cache/apk_wip/res/values/strings.xml.new Dev/device/$TARGET/$all_line/res/values/strings.xml
-			fi
-    		fi
-    		if [ -e .cache/apk_wip/res/values/arrays.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/arrays.xml > .cache/apk_wip/res/values/arrays.xml.new
-			if [ -e .cache/apk_wip/res/values/arrays.xml.new ]; then
-         			cp .cache/apk_wip/res/values/arrays.xml.new Dev/device/$TARGET/$all_line/res/values/arrays.xml
-			fi
-    		fi
-    		if [ -e .cache/apk_wip/res/values/plurals.xml ]; then
-			grep -v ">@" .cache/apk_wip/res/values/plurals.xml > .cache/apk_wip/res/values/plurals.xml.new
-			if [ -e .cache/apk_wip/res/values/plurals.xml.new ]; then
-         			cp .cache/apk_wip/res/values/plurals.xml.new Dev/device/$TARGET/$all_line/res/values/plurals.xml
+    		if [ -e .cache/apk_wip/res/values$ISO/plurals.xml ]; then
+			grep -v ">@" .cache/apk_wip/res/values$ISO/plurals.xml > .cache/apk_wip/res/values$ISO/plurals.xml.new
+			if [ -e .cache/apk_wip/res/values$ISO/plurals.xml.new ]; then
+         			cp .cache/apk_wip/res/values$ISO/plurals.xml.new Dev/$TARGET_DIR/$all_line/res/values$ISO/plurals.xml
 			fi
     		fi
 		echo -en "\r${txtwipe}"
 	fi
 done
-tools/sort.py Dev/device/$TARGET
-echo -e "${txtgrn}Extracted $TARGET XML's${txtrst}"
+$PYTHON tools/sort.py Dev/$TARGET_DIR
+echo -e "${txtgrn}Extracted $TARGET_NAME XML's${txtrst}"
 }
 
-start_extract_xmls () {
+start_extract_main_xmls () {
 # Detect device
 echo -e "${txtblu}Detecting device${txtrst}"
 rm -rf system .cache/system
 unzip -q $ZIP system/build.prop; mv system .cache/system
-TARGET=$(grep ''ro.product.device'=' .cache/system/build.prop | cut -d"=" -f2)
+TARGET=$(grep ''ro.product.name'=' .cache/system/build.prop | cut -d"=" -f2)
+if [ $TARGET == $MAIN_DEVICE ]; then
+	check_zip
+	TARGET_FILE="targets/main.apks"
+	TARGET_DIR="main"
+	TARGET_NAME="main"
+	extract_xmls
+else
+	echo -e "${txtred}Device not a main device${txtrst}: $TARGET"; exit
+fi  
+}
+
+start_extract_device_xmls () {
+# Detect device
+echo -e "${txtblu}Detecting device${txtrst}"
+rm -rf system .cache/system
+unzip -q $ZIP system/build.prop; mv system .cache/system
+TARGET=$(grep ''ro.product.name'=' .cache/system/build.prop | cut -d"=" -f2)
 if [ -e targets/$TARGET.apks ]; then
 	check_zip
-	if [ $TARGET == $MAIN_DEVICE ]; then
-     		extract_xmls_main
-        	extract_xmls_device
-	else
-        	extract_xmls_device
-	fi 
+	TARGET_FILE=targets/$TARGET.apks
+	TARGET_DIR=device/$TARGET
+	TARGET_NAME=$TARGET
+       	extract_xmls
 else
-	echo -e "${txtred}Device not supported${txtrst}: $TARGET"
-	echo -e "Do you want to extract the main xmls?\n${txtred}WARNING:${txtrst} It's recommended to extract main xmls only from Xiaomi devices!"
-        echo -n "(yes,no): "; read device_not_detected
-     	case "$device_not_detected" in
-               yes) extract_xmls_main; exit;;
-		no) exit;;
-        esac
+	echo -e "${txtred}Device not supported${txtrst}: $TARGET"; exit
 fi  
 }
 
 show_argument_help () { 
 echo 
-echo "MIUIAndroid.com XMLS extracter"
+echo "Android XMLS extracter"
 echo 
-echo "Usage: extract.sh --xmls --zip [zip_name]"
+echo "Usage: extract.sh [--main|--device][--zip][zip_name]"
 echo 
 echo " [option]:"
-echo " 		--help		-h			This help"
-echo "		--xmls,		--zip			Extract xmls from specified zip"
+echo " 		--help		-h		This help"
+echo "		--main,	 	--zip		Extract main xmls from specified zip"
+echo "		--device,	--zip		Extract device xmls from specified zip"
 echo 
 exit 
 }
@@ -220,7 +171,7 @@ exit
 if [ $# -gt 0 ]; then
      	if [ $1 == "--help" ]; then
           	show_argument_help
-	elif [ $1 == "--xmls" ]; then
+	elif [ $1 == "--main" ]; then
                 if [ "$2" != "" ]; then
      			if [ "$2" == "--zip" ] || [ $2 == "-z" ]; then
           			if [ -e $3 ]; then
@@ -229,28 +180,30 @@ if [ $# -gt 0 ]; then
                				echo "${txtred}WARNING:${txtrst} zip not specified or not found: aborting"; sleep 1
                		    		exit
           			fi
+          			ISO=$4
 			fi
-
                 else 
-               		echo "zip not specified or not found, using default: update.zip"; sleep 1
+               		echo -e "zip not specified or not found, using default: update.zip"; sleep 1
                		ZIP=update.zip
                 fi
-                start_extract_xmls
-	else
-                if [ "$1" != "" ]; then
-     			if [ "$1" == "--zip" ] || [ $1 == "-z" ]; then
-          			if [ -e $2 ]; then
-               				ZIP=$2
+                start_extract_main_xmls
+	elif [ $1 == "--device" ]; then
+                if [ "$2" != "" ]; then
+     			if [ "$2" == "--zip" ] || [ $2 == "-z" ]; then
+          			if [ -e $3 ]; then
+               				ZIP=$3
           			else
                				echo "${txtred}WARNING:${txtrst} zip not specified or not found: aborting"; sleep 1
                		    		exit
           			fi
+          			ISO=$4
 			fi
-
                 else 
-               		echo "zip not specified or not found, using default: update.zip"; sleep 1
+               		echo -e "zip not specified or not found, using default: update.zip"; sleep 1
                		ZIP=update.zip
                 fi
-                start_extract_xmls
+                start_extract_device_xmls
+	else
+                show_argument_help
       	fi
 fi
